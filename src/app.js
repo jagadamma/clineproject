e0f1d365d115e331cd843c07912bff50958c497a
jagadamma <96439002+jagadamma@users.noreply.github.com>
2025-08-06 21:09:51 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use('/api/user-jobs', jobUserRoutes);
 app.use('/api', courseLibraryRoutes);
 
 app.get("/", (req, res) => {
-    res.send("👋 SWelcome to cliniAura API");
+    res.send("👋 everyone Welcome to cliniAura API");
 });
 
 app.listen(3000, () => {
