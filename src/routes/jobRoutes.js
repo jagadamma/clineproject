5720844363ed9a548e6c52b3a0bf3703176c135d
Soni verma <sonimac@MacBookAir.lan>
2025-08-13 11:19:32 +0530

    some change in jobRoute.js page

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/jobController');
 
-router.post('/EmpjobPost', controller.createJob);            // emploer job
-router.get('/getjobCan', controller.getJobs);               // All jobs (filter/search). //student get job
+router.post('/', controller.createJob);            // emploer job
+router.get('/', controller.getJobs);               // All jobs (filter/search). //student get job
 router.get('/:id', controller.getJobById);         // Single job
 
 module.exports = router;
