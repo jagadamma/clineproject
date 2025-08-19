611590df8fe2e59022156e98becef40d8d0ba51d
jagadamma <96439002+jagadamma@users.noreply.github.com>
2025-08-20 03:11:40 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -82,7 +82,7 @@ app.use('/api/admin/auth', adminAuthRoutes); // POST /login, GET /me
 app.use('/api/admin', adminManageRoutes);    // requires admin auth
 
 app.get("/", (req, res) => {
-    res.send("👋 H WELCOME to suplex & to cliniAura API");
+    res.send("👋 Hi WELCOME to suplex & to cliniAura API");
 });
 app.listen(3000, '0.0.0.0', () => {
     console.log("🚀 Server running on http://0.0.0.0:3000");
